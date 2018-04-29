f7bd7a502070d7dc8fa220c07c32e20e3f217201
Yanan Zhang <yz3054@compute10.cs.columbia.edu>
2018-04-28 22:52:02 -0400

    add q3 results

---
+++ execute_q3_01.py
@@ -0,0 +1,28 @@
+# code for executing mis_pred for different selectivity set.
+
+import sys
+import subprocess
+
+print "The query file name: "+sys.argv[1]
+
+
+params = []
+with open(sys.argv[1], "r") as file:
+	params = []
+	for line in file:
+		l = line.split()
+		#params.append([float(i) for i in l])
+		params.append(l)
+
+print params;
+
+for i in range(0,11):
+	print "==============================================================="
+	print "The", i,"th execution, params:", params[i] 
+	process = subprocess.Popen(["./branch_mispred_q3_01", params[i][0], params[i][1], params[i][2], params[i][3]], stdout=subprocess.PIPE);
+	while True:
+  		line = process.stdout.readline()
+  		if line != '':
+    			print line
+  		else:
+    			break
